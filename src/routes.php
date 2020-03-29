25755577e5684e63b70572775c9cc2510a659c31
Roberto Costa <robertocosta66@gmail.com>
2020-03-29 18:31:30 -0300

    Add post rout in routes.php

--- src/routes.php
+++ src/routes.php
@@ -5,4 +5,5 @@ $router = new Router();
 
 $router->get('/', 'HomeController@index');
 $router->get('/login', 'LoginController@sigin');
+$router->post('/login', 'LoginController@siginAction');
 $router->get('/cadastro', 'LoginController@sigup');
