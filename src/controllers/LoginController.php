b0e6276098a6a870c1fbe864232d966f212bbd10
Roberto Costa <robertocosta66@gmail.com>
2020-03-29 17:05:58 -0300

    Create User.php, LoginController.php and routs of login

---
+++ src/controllers/LoginController.php
@@ -0,0 +1,16 @@
+<?php
+namespace src\controllers;
+
+use \core\Controller;
+
+class LoginController extends Controller {
+    
+    public function sigin() {
+        echo "Tela de login";
+    }
+    
+    public function sigp() {
+        echo "Tela de cadastro";
+    }
+    
+}
