dcf4fd1b46b6e52d19bb509a79014d9cc5963f85
Roberto Costa <robertocosta66@gmail.com>
2020-03-29 20:57:52 -0300

    Create function veriyLogin in LoginHandler.php

--- src/handlers/LoginHandler.php
+++ src/handlers/LoginHandler.php
@@ -9,18 +9,36 @@ class LoginHandler {
         if(!empty($_SESSION['token'])){
             $token = $_SESSION['token'];
 
-            $data = User::select()->where('token', $token)->one();
+            $user = User::select()->where('token', $token)->one();
             
-            if(count($data)>0){
+            if(count($user)>0){
                 $loggedUser = new User();
-               // $loggedUser->setId($data['id']);
-               // $loggedUser->setEmail($data['email']);
-               // $loggedUser->setName($data['name']);
+               // $loggedUser->setId($user['id']);
+               // $loggedUser->setEmail($user['email']);
+               // $loggedUser->setName($user['name']);
 
 
-                return $data;
+                return $user;
             }
         }    
+        return false;
+    }
+
+    public static function verifyLogin($email, $password){
+        //$user = User::select()->where('email',$email && 'password',$password)->one();
+        $user = User::select()->where('email')->one();
+
+        if($user){
+            if(password_verify($password, $user['password'])){
+                //Gerando token
+                $token = md5(time().rand(0,9999).time());
+                USER::update()
+                    ->set('token', $token)
+                    ->where('id',$user['id'])
+                ->execute();
+                return $token;
+            }
+        }
         return false;
     }
 }
