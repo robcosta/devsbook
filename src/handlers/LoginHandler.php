b6617ddbe0e15bcd822b069d75f2cb05b1cc1f61
Roberto Costa <robertocosta66@gmail.com>
2020-03-30 16:03:49 -0300

    Create emailExists an addUser in LoginHandler.php

--- src/handlers/LoginHandler.php
+++ src/handlers/LoginHandler.php
@@ -10,7 +10,7 @@ class LoginHandler {
             $token = $_SESSION['token'];
 
             $user = User::select()->where('token', $token)->one();
-            
+                       
             if(count($user)>0){
                 $loggedUser = new User();
                // $loggedUser->setId($user['id']);
@@ -26,7 +26,7 @@ class LoginHandler {
 
     public static function verifyLogin($email, $password){
         //$user = User::select()->where('email',$email && 'password',$password)->one();
-        $user = User::select()->where('email')->one();
+        $user = User::select()->where('email',$email)->one();
 
         if($user){
             if(password_verify($password, $user['password'])){
@@ -40,5 +40,25 @@ class LoginHandler {
             }
         }
         return false;
+    }
+
+    public static function emailExists($email){
+        $user = User::select()->where('email', $email)->one();
+        return $user ? true : false;
+    }
+
+    public static function addUser($name, $email, $password, $birthDate){
+        //Gera o hash da senha
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $token = md5(time().rand(0,9999).time());
+        User::insert([
+            'email' => $email,
+            'password' => $hash,
+            'name' => $name,
+            'birthdate' => $birthDate,
+            'token' => $token
+        ])->execute();
+
+        return$token;
     }
 }
