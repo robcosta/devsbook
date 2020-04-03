89cb4b8c2e30b501f39adb727464e4a140701a51
Roberto Costa <robertocosta66@gmail.com>
2020-04-03 11:27:11 -0300

    Create getHomeFeed in PostHandler

--- src/handlers/PostHandler.php
+++ src/handlers/PostHandler.php
@@ -1,6 +1,8 @@
 <?php
 namespace src\handlers;
 use \src\models\Post;
+use \src\models\User;
+use \src\models\UserRelations;
 
 class PostHandler {
 
@@ -17,6 +19,57 @@ public static function addPost($idUser, $type, $body, $date){
     }
 
     return true;
+}
+
+
+public static function getHomeFeed($idUser, $page){
+    $perPage = 2;
+    // 1. Pegar a lista de usuarios que Eu sigo
+    $userList = UserRelations::select()->where('id',$idUser)->get();
+    $users = [];
+    foreach ($userList as $userItem) {
+        $users = $userItem->user_to; 
+    }
+    $users[] = $idUser;
+    
+    // 2. Pegar os posts dessa galera ordenado pela data.
+    $postList = Post::select()
+        ->where('id_user','in',$users)
+        ->orderby('created_at','desc')
+        ->page($page, $perPage)
+    ->get();
+
+    $total = Post::select()
+        ->where('id_user','in',$users)
+    ->count();
+
+    $pageCount = ceil($total/$perPage);
+
+    // 3. Transformar o resultado em objetos dos models (Post).
+    $posts = [];
+    foreach ($postList as $postItem) {
+        $newPost = new Post();
+        $newPost->id = $postItem['id'];
+        $newPost->type = $postItem['type'];
+        $newPost->createdAt = date('d/m/Y H:i:s', strtotime($postItem['created_at']));
+        $newPost->body = $postItem['body'];
+        // 4. Preencher as informações adicionais no post.
+        $newUser = User::select()->where('id',$postItem['id_user'])->one();
+        $newPost->user = new User();
+        $newPost->user->id = $newUser['id'];
+        $newPost->user->name = $newUser['name'];
+        $newPost->user->avatar = $newUser['avatar'];
+        // TODO: 4.1 Preencher informações de LIKE
+        // TODO: 4.2 Preenche informações de COMENTS
+
+        $posts[] = $newPost;
+    }
+    // 5. Retornar. 
+    return [
+        'posts' => $posts,
+        'pageCount' => $pageCount,
+        'currentPage' => $page
+    ];
 }
 
 }
