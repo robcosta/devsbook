a432d2fc1f44fe7be2a513a655e48c45284256f2
Roberto Costa <robertocosta66@gmail.com>
2020-04-03 11:23:34 -0300

    Changes to feed-item.php to show my posts and the users I follow.

--- src/views/partials/feed-item.php
+++ src/views/partials/feed-item.php
@@ -2,23 +2,31 @@
     <div class="box-body">
         <div class="feed-item-head row mt-20 m-width-20">
             <div class="feed-item-head-photo">
-                <a href=""><img src="media/avatars/avatar.jpg" /></a>
+                <a href=""><img src="<?=$base;?>/media/avatars/<?=$data->user->avatar;?>" /></a>
             </div>
             <div class="feed-item-head-info">
-                <a href=""><span class="fidi-name">Bonieky Lacerda</span></a>
-                <span class="fidi-action">fez um post</span>
+                <a href=""><span class="fidi-name"><?=$data->user->name;?></span></a>
+                <span class="fidi-action">
+                    <?php switch ($data->type) {
+                        case 'text':
+                            echo "fez um post.";
+                            break;
+                        
+                        case 'photo':
+                            echo "postou uma foto";
+                            break;
+                    } ?>                 
+                </span>
                 <br/>
-                <span class="fidi-date">07/03/2020</span>
+                <span class="fidi-date"><?=$data->createdAt;?></span>
             </div>
             <div class="feed-item-head-btn">
-                <img src="assets/images/more.png" />
+                <img src="<?=$base;?>/assets/images/more.png" />
             </div>
         </div>
-        <div class="feed-item-body mt-10 m-width-20">
-            Pessoal, tudo bem! Busco parceiros para empreender comigo em meu software.<br/><br/>
-            Acabei de aprová-lo na Appstore. É um sistema de atendimento via WhatsApp multi-atendentes para auxiliar empresas.<br/><br/>
-            Este sistema permite que vários funcionários/colaboradores da empresa atendam um mesmo número de WhatsApp, mesmo que estejam trabalhando remotamente, sendo que cada um acessa com um login e senha particular....
-        </div>
+        <div class="feed-item-body mt-10 m-width-20"><?=
+        //function nl2br(string) do PHP quebra a linha de texto conforme a astring
+        nl2br($data->body);?></div>
         <div class="feed-item-buttons row mt-20 m-width-20">
             <div class="like-btn on">56</div>
             <div class="msg-btn">3</div>
@@ -34,7 +42,7 @@
                     Comentando no meu próprio post
                 </div>
             </div>
-
+            <!--        
             <div class="fic-item row m-height-10 m-width-20">
                 <div class="fic-item-photo">
                     <a href=""><img src="media/avatars/avatar.jpg" /></a>
@@ -51,7 +59,7 @@
                 </div>
                 <input type="text" class="fic-item-field" placeholder="Escreva um comentário" />
             </div>
-
+            -->
         </div>
     </div>
 </div>
