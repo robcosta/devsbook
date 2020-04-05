2363695dedc97d69aa4e225ffc48b804d3e267c1
Roberto Costa <robertocosta66@gmail.com>
2020-04-05 18:40:59 -0300

    Added more features in index of ProfileController

--- src/controllers/ProfileController.php
+++ src/controllers/ProfileController.php
@@ -18,9 +18,25 @@ class ProfileController extends Controller {
     }
 
     public function index($atts = []){
+        $id = $this->loggedUser->id;
+        if(isset($atts['id'])){
+            $id = $atts['id']; 
+        }
+        $user = UserHandler::getUser($id, true);
+        if(!$user){
+            $this->redirect("/");
+        }
         
+        //Cálculo da idade
+        $dateFrom = new \DateTime($user->birthdate);
+        $dateTo = new \DateTime('today');
+
+        $user->ageYears = $dateFrom->diff($dateTo)->y;
+
+
         $this->render('profile',[
-            'loggedUser' => $this->loggedUser
+            'loggedUser' => $this->loggedUser,
+            'user' => $user
         ]);
     }
 }
