5d5a997a97d84d8ad1c3dcbb7224cb620873eda1
Roberto Costa <robertocosta66@gmail.com>
2020-04-15 17:44:14 -0300

    Created and linked 'friends' routes

--- src/routes.php
+++ src/routes.php
@@ -14,13 +14,15 @@ $router->post('/signup', 'LoginController@signupAction');
 
 $router->post('/post/new', 'PostController@new');
 
+$router->get('/perfil/{id}/amigos','ProfileController@friends');
 $router->get('/perfil/{id}/follow','ProfileController@follow');
 $router->get('/perfil/{id}','ProfileController@index');
 $router->get('/perfil','ProfileController@index');
+$router->get('/perfil/amigos','ProfileController@friends');
 
 $router->get('/sair','LoginController@logout');
+
 //$router->get('/pesquisa', 'HomeController@search');
 //$router->get('/perfil);
-//$router->get('/amigos);
 //$router->get('/fotos);
 //$router->get('/config);
